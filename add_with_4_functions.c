0eac3d624519ccf1f961625e74748b4ebaf957fa
ENG19CS0010-ABHIJITHJK <76121346+ENG19CS0010-ABHIJITHJK@users.noreply.github.com>
2021-02-21 22:37:08 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -2,27 +2,27 @@
 #include<stdio.h>
 int input()
 {
-int n1;
-printf("Enter number: \n");
-scanf("%d", &n1);
-return n1;
+  int n1;
+  printf("Enter number: \n");
+  scanf("%d", &n1);
+  return n1;
 }
 int sum(int n1, int n2)
 {
-int sum;
-sum=n1+n2;
-return sum;
+  int sum;
+  sum=n1+n2;
+  return sum;
 }
-int output(int n1, int n2, int result)
+void output(int n1, int n2, int result)
 {
-printf("Sum of %d and %d is %d", n1,n2,result);
+  printf("Sum of %d and %d is %d", n1,n2,result);
 }
 int main()
 {
-int n1,n2,res;
-n1 = input();
-n2 = input();
-res = sum(n1,n2);
-output(n1,n2,res);
-return 0;
+  int n1,n2,res;
+  n1 = input();
+  n2 = input();
+  res = sum(n1,n2);
+  output(n1,n2,res);
+  return 0;
 }
