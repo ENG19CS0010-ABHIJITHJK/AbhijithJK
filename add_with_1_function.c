93cbc8a40cbf3f32271f1cbabd13643dfced6252
ENG19CS0010-ABHIJITHJK <76121346+ENG19CS0010-ABHIJITHJK@users.noreply.github.com>
2021-02-21 22:28:55 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
-printf("Enter number: ");
-scanf("%d", &a);
-printf("Enter number: ");
-scanf("%d", &b);
-c= a+b;
-printf("Sum of %d and %d is %d \n", a, b, c);
-return 0;
+  int a,b,c;
+  printf("Enter number: ");
+  scanf("%d", &a);
+  printf("Enter number: ");
+  scanf("%d", &b);
+  c= a+b;
+  printf("Sum of %d and %d is %d \n", a, b, c);
+  return 0;
 }
