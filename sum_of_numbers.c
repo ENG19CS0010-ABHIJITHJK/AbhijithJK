0a2ac025bc80c64655e9e10eabf22c4e2df65322
ENG19CS0010-ABHIJITHJK <76121346+ENG19CS0010-ABHIJITHJK@users.noreply.github.com>
2021-03-31 20:02:22 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,15 +1,16 @@
 //Write a program to find the sum of n different numbers using 4 functions
 #include <stdio.h>
-int a[50],coun,sum=0;
 
 int countt()
 {
+    int coun;
     printf("Enter the count ");
     scanf("%d",&coun);
     return coun;
 }
 int input(int coun)
 {
+    int a[50],sum=0;
     printf("Enter the Numbers ");
     for(int i =0;i<coun;i++)
     {
@@ -21,7 +22,7 @@ int input(int coun)
     }
     return sum;
 }
-int display()
+int display(int sum)
 {
     printf("The sum of the given numbers is %d",sum);
 }
